3c04f6fa4278114dd49168e574a21a34e2d6f524
Mukesh Kumar Padhi <124678015+Mukeshkup@users.noreply.github.com>
2024-02-20 09:13:51 +0530

    Add files via upload

---
+++ 9. Palindrome Number/palindrome.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class palindrome {
+    public static boolean isPalindrome(int x) {
+        int rem,n=x,rev=0;
+        while(x>0){
+            rem=x%10;
+            rev=rev*10+rem;
+            x=x/10;
+        }
+        if (rev==n){
+            return true;
+        }
+        return false;
+    }
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int n = sc.nextInt();
+        if(isPalindrome(n)==true){
+            System.out.println("Palindrome");
+        }
+        else
+            System.out.println("Not Palindrome");
+         
+
+    }
+}
